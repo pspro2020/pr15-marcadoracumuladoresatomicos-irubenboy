aa094f1992c11e2544305bfd6eee85a4d6c4c5ba
Rubeb <rubenmoreno.saladillo@gmail.com>
2020-11-18 13:38:01 +0100

    Resolución del problema

--- src/classes/Dice.java
+++ src/classes/Dice.java
@@ -1,34 +1,36 @@
 package classes;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
 
 public class Dice{
-    private AtomicLong ai1 = new AtomicLong(0);
-    private AtomicLong ai2 = new AtomicLong(0);
-    private AtomicLong ai3 = new AtomicLong(0);
-    private AtomicLong ai4 = new AtomicLong(0);
-    private AtomicLong ai5 = new AtomicLong(0);
-    private AtomicLong ai6 = new AtomicLong(0);
-    private AtomicLong[] result = {ai1,ai2,ai3,ai4,ai5,ai6};
-    private Object[] watcher = {new Object(), new Object(), new Object(), new Object(), new Object(), new Object()};
+
+    private LongAdder[] result = {
+            new LongAdder(),
+            new LongAdder(),
+            new LongAdder(),
+            new LongAdder(),
+            new LongAdder(),
+            new LongAdder()
+    };
 
     @Override
     public String toString() {
         String s = "";
         int total = 0;
         for (int i = 0; i < result.length; i++) {
-            s += String.format("Numero %d: %d veces\n", i+1, result[i].get());
-            total += result[i].get();
+            s += String.format("Numero %d: %d veces\n", i+1, result[i].longValue());
+            total += result[i].longValue();
         }
 
-        s += String.format("Total de veces: %d + %d + %d + %d + %d + %d = %d\n", result[0].get(), result[1].get(),
-                result[2].get(), result[3].get(), result[4].get(), result[5].get(), total);
+        s += String.format("Total de veces: %d + %d + %d + %d + %d + %d = %d\n", result[0].longValue(), result[1].longValue(),
+                result[2].longValue(), result[3].longValue(), result[4].longValue(), result[5].longValue(), total);
 
         return s;
     }
 
     public void throwDice(int number){
-        result[number-1].addAndGet(1);
+        result[number-1].add(1);
 
         System.out.println("Se ha lanzado el número " + number);
 
